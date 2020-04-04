3dee0116be2c76748db7293433d456950e607784
nintervik <vmasogarcia@gmail.com>
2020-04-04 12:48:44 +0200

    generalized code for offsetting keyframes

--- vmWalkingKitFiles/libraryUI.py
+++ vmWalkingKitFiles/libraryUI.py
@@ -437,25 +437,7 @@ class WalkLibraryUI(QtWidgets.QWidget):
         # Calculate the playback range according to the current indices retrieved above. Body and arms beat dictate
         # the length of the whole walk cycle animation. This allows for a minimum playback time while always allowing
         # the animation to be looped properly.
-        playBackEndRange = 0
-
-        if indices[0] == 1 and indices[1] == 1:
-            playBackEndRange = 16
-        elif (indices[0] == 1 and indices[1] == 2) or (indices[0] == 2 and indices[1] == 1):
-            playBackEndRange = 48
-        elif ((indices[0] == 1 and indices[1] == 3) or (indices[0] == 3 and indices[1] == 1))\
-                or (indices[0] == 3 and indices[1] == 3):
-            playBackEndRange = 32
-        elif indices[0] == 2 and indices[1] == 2:
-            playBackEndRange = 24
-        elif (indices[0] == 2 and indices[1] == 3) or (indices[0] == 3 and indices[1] == 2):
-            playBackEndRange = 96
-
-        # Set de new calculated playback range
-        cmds.playbackOptions(animationEndTime=96)
-        cmds.playbackOptions(minTime=1)
-        cmds.playbackOptions(maxTime=playBackEndRange)
-        cmds.playbackOptions(animationStartTime=1)
+        self.calculatePlaybackRange(indices)
 
     def onDropDownBodyBeatChanged(self, prefix, index):
         """
@@ -502,63 +484,18 @@ class WalkLibraryUI(QtWidgets.QWidget):
         # Calculate the playback range according to the current indices retrieved above. Body and arms beat dictate
         # the length of the whole walk cycle animation. This allows for a minimum playback time while always allowing
         # the animation to be looped properly.
-        playBackEndRange = 0
-
-        if indices[0] == 1 and indices[1] == 1:
-            playBackEndRange = 16
-        elif (indices[0] == 1 and indices[1] == 2) or (indices[0] == 2 and indices[1] == 1):
-            playBackEndRange = 48
-        elif ((indices[0] == 1 and indices[1] == 3) or (indices[0] == 3 and indices[1] == 1))\
-                or (indices[0] == 3 and indices[1] == 3):
-            playBackEndRange = 32
-        elif indices[0] == 2 and indices[1] == 2:
-            playBackEndRange = 24
-        elif (indices[0] == 2 and indices[1] == 3) or (indices[0] == 3 and indices[1] == 2):
-            playBackEndRange = 96
+        self.calculatePlaybackRange(indices)
 
         # Query the current BodyBeat index
         currBodyIndex = self.paramWidgets[prefix].currentIndex() + 1
 
         if currBodyIndex is not None:
+            # Create the ty attribute of the controller that handles the up and down body movement
+            attrGeneralUpDown = 'Mr_Buttons:Mr_Buttons_COG_Ctrl.translateY'
+            self.offsetKeyFrames(attrGeneralUpDown, 'UpDown_1', currBodyIndex)
 
-            # Create the ty attribute of the controller that handles the up and down movement
-            cntrlName = 'Mr_Buttons:Mr_Buttons_COG_Ctrl'
-            attrFull = '%s.%s' % (cntrlName, 'translateY')
-            offset = 0
-
-            # Calculate the offset of the keyframes that need to be moved according to the current and previous index
-            # of the BodyBeat parameter
-            if (self.prevBodyIndex == 1 and currBodyIndex == 2) or (self.prevBodyIndex == 2 and currBodyIndex == 3):
-                offset = 1
-            elif (self.prevBodyIndex == 2 and currBodyIndex == 1) or (self.prevBodyIndex == 3 and currBodyIndex == 2):
-                offset = -1
-            elif self.prevBodyIndex == 1 and currBodyIndex == 3:
-                offset = 2
-            elif self.prevBodyIndex == 3 and currBodyIndex == 1:
-                offset = -2
-
-            # Select the UpDown layer so its keyframes can be moved
-            cmds.animLayer('UpDown_1', edit=True, selected=True)
-
-            # Query the current keyframes in Translate Y
-            keyframes = cmds.keyframe(attrFull, query=True)
-
-            # For each of the current keyframes move them the 'offset' amount. Except for the frame 1 that will be
-            # always at the same position
-            for i in range(0, len(keyframes)):
-                if i != 0:
-                    # Every time before moving, query again the current keyframes as they are now moved
-                    keyframes = cmds.keyframe(attrFull, query=True)
-                    # Move the keyframes the 'offset' amount in the range from the current one to the last one
-                    cmds.keyframe(attrFull, edit=True, relative=True,
-                                  timeChange=offset, time=(keyframes[i],
-                                  keyframes[len(keyframes)-1]))
-
-        # Set de new calculated playback range
-        cmds.playbackOptions(animationEndTime=96)
-        cmds.playbackOptions(minTime=1)
-        cmds.playbackOptions(maxTime=playBackEndRange)
-        cmds.playbackOptions(animationStartTime=1)
+            attrHeadUpDown = 'Mr_Buttons:Mr_Buttons_Head_01FKCtrl.translateY'
+            self.offsetKeyFrames(attrHeadUpDown, 'HeadUpDown_1', currBodyIndex)
 
         # Store the previous BodyBeat index for the next calculation
         self.prevBodyIndex = self.paramWidgets[prefix].currentIndex() + 1
@@ -648,6 +585,61 @@ class WalkLibraryUI(QtWidgets.QWidget):
         else:
             logger.debug("If a directory is given a name must be given as well.")
 
+    # KEYFRAMES METHODS
+
+    def offsetKeyFrames(self, attrFull, layerName, currBodyIndex):
+
+        offset = 0
+
+        # Calculate the offset of the keyframes that need to be moved according to the current and previous index
+        # of the BodyBeat parameter
+        if (self.prevBodyIndex == 1 and currBodyIndex == 2) or (self.prevBodyIndex == 2 and currBodyIndex == 3):
+            offset = 1
+        elif (self.prevBodyIndex == 2 and currBodyIndex == 1) or (self.prevBodyIndex == 3 and currBodyIndex == 2):
+            offset = -1
+        elif self.prevBodyIndex == 1 and currBodyIndex == 3:
+            offset = 2
+        elif self.prevBodyIndex == 3 and currBodyIndex == 1:
+            offset = -2
+
+        # Select the layer so its keyframes can be moved
+        cmds.animLayer(layerName, edit=True, selected=True)
+
+        # Query the current keyframes in the given attribute
+        keyframes = cmds.keyframe(attrFull, query=True)
+
+        # For each of the current keyframes move them the 'offset' amount. Except for the frame 1 that will always
+        # be at the same position
+        for i in range(0, len(keyframes)):
+            if i != 0:
+                # Every time before moving, query again the current keyframes as they are now moved
+                keyframes = cmds.keyframe(attrFull, query=True)
+                # Move the keyframes the 'offset' amount in the range from the current one to the last one
+                cmds.keyframe(attrFull, edit=True, relative=True,
+                              timeChange=offset, time=(keyframes[i],
+                              keyframes[len(keyframes)-1]))
+
+    def calculatePlaybackRange(self, indices):
+
+        playBackEndRange = 0
+
+        if indices[0] == 1 and indices[1] == 1:
+            playBackEndRange = 16
+        elif (indices[0] == 1 and indices[1] == 2) or (indices[0] == 2 and indices[1] == 1):
+            playBackEndRange = 48
+        elif ((indices[0] == 1 and indices[1] == 3) or (indices[0] == 3 and indices[1] == 1))\
+                or (indices[0] == 3 and indices[1] == 3):
+            playBackEndRange = 32
+        elif indices[0] == 2 and indices[1] == 2:
+            playBackEndRange = 24
+        elif (indices[0] == 2 and indices[1] == 3) or (indices[0] == 3 and indices[1] == 2):
+            playBackEndRange = 96
+
+        # Set de new calculated playback range
+        cmds.playbackOptions(animationEndTime=96)
+        cmds.playbackOptions(minTime=1)
+        cmds.playbackOptions(maxTime=playBackEndRange)
+        cmds.playbackOptions(animationStartTime=1)
 
 # MAYA WINDOWS FUNCTIONS
 
